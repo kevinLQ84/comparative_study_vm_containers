4fcec2a26b84bad43fa78a995c2d819b58161af9
kevinLQ84 <kevinlq84i@gmail.com>
2023-10-12 01:47:25 +0100

    first commit

---
+++ runtime_data_finaliser.py
@@ -0,0 +1,12 @@
+'''
+Program to take in JSON files from 'runtime_data_gatherer'.
+Output graphs with other consolidated data, such as mean, median, mode, range and many others.
+Consideration of output values:
+    - graph data of various execution times
+    - mean
+    - median
+    - mode
+    - range
+    - highest execution time
+    - lowest execution time
+'''
---
+++ runtime_data_gatherer.py
@@ -0,0 +1,5 @@
+'''
+program to use a specified list of python programs listed inside.
+there should be a default seed to test random numbers
+execution output should be placed in a folder in JSON formatting
+'''
